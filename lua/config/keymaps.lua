b2b79a31ea99a84f7bc0bca34a8695541d88cd08
David Rainsford <david@rainsford.org>
2025-03-18 23:50:38 +1100

    Syntactic sugar

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,16 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+local set = vim.keymap.set
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- toggle the number and sign columns
-vim.keymap.set('n', '<leader>n', function()
+set('n', '<leader>n', function()
   if vim.wo.number then
     vim.wo.number = false
     vim.wo.signcolumn = 'no'
@@ -20,14 +22,14 @@ vim.keymap.set('n', '<leader>n', function()
   end
 end, { desc = 'Toggle line numbers and sign column' })
 
-vim.keymap.set('n', '<leader>w', function()
+set('n', '<leader>w', function()
   vim.wo.wrap = not vim.wo.wrap
 end, { noremap = true, silent = true, desc = 'Toggle Word Wrap' })
 
 -- move virtually up and down wrapped lines, but use physical lines when used with a count
-vim.keymap.set('n', 'j', function()
+set('n', 'j', function()
   return vim.v.count == 0 and 'gj' or 'j'
 end, { expr = true, noremap = true, silent = true })
-vim.keymap.set('n', 'k', function()
+set('n', 'k', function()
   return vim.v.count == 0 and 'gk' or 'k'
 end, { expr = true, noremap = true, silent = true })

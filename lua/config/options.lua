27c3cb9148ad90a4b10cff64e07bae6216d74ca1
David Rainsford <david@rainsford.org>
2025-03-18 14:27:29 +1100

    Code cleanup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,54 +1,19 @@
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+local opt = vim.opt
 
--- Make line numbers default
-vim.opt.number = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = false
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = false
+opt.confirm = false
+opt.cursorline = true
+opt.ignorecase = true -- foo matches foo Foo FOO
+opt.inccommand = 'split'
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.mouse = 'a'
+opt.number = true
+opt.scrolloff = 10
+opt.showmode = false
+opt.signcolumn = 'yes'
+opt.smartcase = true -- Foo matches Foo
+opt.splitbelow = true
+opt.splitright = true
+opt.swapfile = false
+opt.timeoutlen = 300 -- time in ms to wait for a mapped sequence to complete (default 1000)
+opt.undofile = false

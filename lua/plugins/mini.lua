0ac30425bb99535cd2bba0946d668a86605ec844
David Rainsford <david@rainsford.org>
2025-03-18 20:34:18 +1100

    <leader>e to open file browser

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,6 +16,11 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
+    require('mini.files').setup()
+    vim.keymap.set('n', '<leader>e', function()
+      MiniFiles.open()
+    end)
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
